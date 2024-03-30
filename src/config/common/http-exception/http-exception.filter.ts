dd5eeaad1237935d20473ac6ac31bca5eabcd222
Chris-AM <c.aranguizm@outlook.com>
2024-03-30 20:01:51 -0300

    :sparkles: defining common config layer

--- src/config/common/http-exception/http-exception.filter.ts
+++ src/config/common/http-exception/http-exception.filter.ts
@@ -1,6 +1,41 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { LoggerService } from 'src/config/logger/logger.service';
 
 @Catch()
-export class HttpExceptionFilter<T> implements ExceptionFilter {
-  catch(exception: T, host: ArgumentsHost) {}
+export class HttpExceptionFilter implements ExceptionFilter {
+  constructor(private readonly logger: LoggerService) {
+    this.logger.service('HttpExceptionFilter');
+  }
+  catch(exception: any, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const response = context.getResponse();
+    const request = context.getRequest();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : new InternalServerErrorException('Internal Server Error');
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : exception.message;
+
+    this.logger.error(
+      'HttpExceptionFilter',
+      `Error: ${message}, Status: ${status}`,
+    );
+
+    response.status(status).json({
+      time: new Date().toISOString(),
+      path: request.url,
+      method: request.method,
+      status: status,
+      error: message,
+    });
+  }
 }
--- src/config/common/pagination-dto/pagination-dto.ts
+++ src/config/common/pagination-dto/pagination-dto.ts
@@ -1,1 +1,32 @@
-export class PaginationDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional, IsPositive, Min } from 'class-validator';
+
+export class PaginationDto {
+  @ApiProperty({
+    required: false,
+    default: 10,
+  })
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  @Min(1)
+  @Type(() => Number)
+  limit?: number;
+
+  @ApiProperty({
+    required: false,
+    default: 0,
+  })
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  @Min(0)
+  @Type(() => Number)
+  offset?: number;
+
+  constructor(limit = 10, offset = 0) {
+    this.limit = limit;
+    this.offset = offset;
+  }
+}
