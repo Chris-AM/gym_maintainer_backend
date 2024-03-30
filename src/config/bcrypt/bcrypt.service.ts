c9be22d15651d26a60c54548aa20f96d547ef2fa
Chris-AM <c.aranguizm@outlook.com>
2024-03-30 19:04:12 -0300

    :sparkles: defining bcrypt config layer

--- src/config/bcrypt/bcrypt.service.ts
+++ src/config/bcrypt/bcrypt.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { IBcryptService } from './bcrypt.interface';
 
 @Injectable()
-export class BcryptService {}
+export class BcryptService implements IBcryptService {
+  private readonly numberOfRounds: number = 10;
+  async hashPassword(password: string): Promise<string> {
+    const hashedPassword = await bcrypt.hash(password, this.numberOfRounds);
+    return hashedPassword;
+  }
+
+  async comparePassword(password: string, hash: string): Promise<boolean> {
+    const doesPassMatch = await bcrypt.compare(password, hash);
+    return doesPassMatch;
+  }
+}
