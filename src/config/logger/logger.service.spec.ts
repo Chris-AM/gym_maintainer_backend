89b38933c69818f322a0259917e8de7d46a4e497
Chris-AM <c.aranguizm@outlook.com>
2024-03-30 19:36:09 -0300

    :sparkles: defining logger config layer

--- src/config/logger/logger.service.spec.ts
+++ src/config/logger/logger.service.spec.ts
@@ -15,4 +15,48 @@ describe('LoggerService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should log a message', () => {
+    const context = 'Test';
+    const message = 'Test message';
+    expect(service.log(context, message)).toBeUndefined();
+  });
+
+  it('should log an error', () => {
+    const context = 'Test';
+    const message = 'Test message';
+    const trace = 'Test trace';
+    expect(service.error(context, message, trace)).toBeUndefined();
+  });
+
+  it('should log a warning', () => {
+    const context = 'Test';
+    const message = 'Test message';
+    expect(service.warn(context, message)).toBeUndefined();
+  });
+
+  it('should log a verbose message', () => {
+    const context = 'Test';
+    const message = 'Test message';
+    expect(service.verbose(context, message)).toBeUndefined();
+  });
+
+  it('should log a service message', () => {
+    const context = 'Test';
+    expect(service.service(context)).toBeUndefined();
+  });
+
+  it('should NOT log a message in production', () => {
+    process.env.NODE_ENV = 'production';
+    const context = 'Test';
+    const message = 'Test message';
+    expect(service.log(context, message)).toBeUndefined();
+  });
+
+  it('should LOG a message when not in production', () => {
+    process.env.NODE_ENV = 'local';
+    const context = 'Test';
+    const message = 'Test message';
+    expect(service.log(context, message)).toBeUndefined();
+  });
 });
--- src/config/logger/logger.service.ts
+++ src/config/logger/logger.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ILogger } from './logger.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class LoggerService {}
+export class LoggerService extends Logger implements ILogger {
+  constructor(private readonly config: ConfigService) {
+    super();
+  }
+  service(context: string): void {
+    super.log(`[SERVICE] | Context => [${context}]`);
+  }
+  log(context: string, message: string): void {
+    super.log(`[LOG] | Context => [${context}] | Message => [${message}]`);
+  }
+  error(context: string, message: string, trace?: string): void {
+    super.error(
+      `[ERROR] | Context => [${context}] | Message => [${message}] | Trace => [${trace}]`,
+    );
+  }
+  warn(context: string, message: string): void {
+    super.warn(`[WARN] | Context => [${context}] | Message => [${message}]`);
+  }
+  verbose(context: string, message: string): void {
+    if (this.config.get<string>('NODE_ENV') !== 'production') {
+      super.verbose(
+        `[VERBOSE] | Context => [${context}] | Message => [${message}]`,
+      );
+    }
+  }
+}
