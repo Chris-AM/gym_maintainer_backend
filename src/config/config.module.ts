1d9ac4ee94c95b151330427daae33faf8771c317
Chris-AM <c.aranguizm@outlook.com>
2024-03-30 18:53:50 -0300

    :construction: declaring some other config components

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerService } from './logger/logger.service';
+import { BcryptService } from './bcrypt/bcrypt.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
-  providers: [],
+  providers: [LoggerService, BcryptService],
   exports: [],
 })
 export class EnvironmentConfigModule {}
