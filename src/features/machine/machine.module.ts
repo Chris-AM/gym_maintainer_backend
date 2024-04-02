8e3ad8f93f02bdfcc7a53df0d460402d19270b5d
Chris-AM <c.aranguizm@outlook.com>
2024-04-02 14:14:53 -0300

    :bug: solving db schema not created bug

--- src/features/machine/machine.module.ts
+++ src/features/machine/machine.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MachineService } from './use-case/machine.service';
 import { MachineController } from './use-case/machine.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MachineEntity, MachineSchema } from './domain/machine.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: MachineEntity.name,
+        useFactory: () =>
+          MachineSchema.plugin(require('mongoose-autopopulate')),
+      },
+    ]),
+  ],
   controllers: [MachineController],
   providers: [MachineService],
 })
