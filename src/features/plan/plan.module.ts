608bf77cce044ab3565ed10d08ef1cf28df0ee81
Chris-AM <c.aranguizm@outlook.com>
2024-04-02 15:31:12 -0300

    :bug: solving db schema not created bug

--- src/features/plan/plan.module.ts
+++ src/features/plan/plan.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PlanService } from './use-case/plan.service';
 import { PlanController } from './use-case/plan.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PlanEntity, PlanSchema } from './domain/plan.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: PlanEntity.name,
+        useFactory: () => PlanSchema.plugin(require('mongoose-autopopulate')),
+      },
+    ]),
+  ],
   controllers: [PlanController],
   providers: [PlanService],
 })
