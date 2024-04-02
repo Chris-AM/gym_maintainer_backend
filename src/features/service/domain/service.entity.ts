a3f51b8c10afa2c8e9b0855cc3ee1549083cd73e
Chris-AM <c.aranguizm@outlook.com>
2024-04-02 16:19:53 -0300

    :sparkles: service component domain ready

--- src/features/service/domain/service.entity.ts
+++ src/features/service/domain/service.entity.ts
@@ -1,1 +1,36 @@
-export class ServiceEntity {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { CoachEntity } from 'src/features/users/base-user/worker-user/coach/domain/coach.entity';
+import { v4 } from 'uuid';
+
+export type ServiceDocument = ServiceEntity & Document;
+
+@Schema({
+  timestamps: true,
+  collection: 'service',
+})
+export class ServiceEntity {
+  @Prop({ unique: true, type: String, default: v4 })
+  serviceId: string;
+  @Prop({ required: true })
+  name: string;
+  @Prop({ required: true })
+  description: string;
+  @Prop({ required: true })
+  price: number;
+  @Prop({ required: true })
+  duration: number;
+  @Prop({ required: true, type: Boolean, default: true })
+  isActive: boolean;
+  @Prop({ required: true, type: Number })
+  daysAWeek: number;
+  @Prop({ required: true, type: CoachEntity, ref: 'coachUser' })
+  chief: CoachEntity;
+}
+
+const ServiceSchema = SchemaFactory.createForClass(ServiceEntity);
+ServiceSchema.virtual('id').get(function (this: ServiceDocument) {
+  return this._id;
+});
+
+export { ServiceSchema };
--- src/features/service/service.module.ts
+++ src/features/service/service.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ServiceService } from './use-case/service.service';
 import { ServiceController } from './use-case/service.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServiceEntity, ServiceSchema } from './domain/service.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: ServiceEntity.name,
+        useFactory: () =>
+          ServiceSchema.plugin(require('mongoose-autopopulate')),
+      },
+    ]),
+  ],
   controllers: [ServiceController],
   providers: [ServiceService],
 })
