9721c8430f87673d991b4622e8d738ceaa9bd0dc
Chris-AM <c.aranguizm@outlook.com>
2024-04-02 15:22:39 -0300

    :sparkles: plan component domain ready

--- src/features/plan/domain/dto/create-plan.dto.ts
+++ src/features/plan/domain/dto/create-plan.dto.ts
@@ -1,1 +1,54 @@
-export class CreatePlanDto {}
+import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+export class CreatePlanDto {
+  @ApiProperty({
+    description: 'Nombre del Plan',
+    type: String,
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    description: 'Descripción del Plan',
+    type: String,
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @ApiProperty({
+    description: 'Precio del Plan',
+    type: Number,
+    required: true,
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+
+  @ApiProperty({
+    description: 'Duración del Plan',
+    type: Number,
+    required: true,
+    example: '1 <- 1 mes, etc.',
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  duration: number;
+
+  @ApiProperty({
+    description: 'Días a la semana que se realizará el plan',
+    type: Number,
+    required: true,
+    example: '1 <- 1 vez a la semana, etc.',
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  daysAWeek: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  excercises: string[];
+}
--- src/features/plan/domain/plan.entity.ts
+++ src/features/plan/domain/plan.entity.ts
@@ -1,1 +1,49 @@
-export class PlanEntity {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { ExcerciseEntity } from 'src/features/excercise/domain/excercise.entity';
+import { v4 } from 'uuid';
+
+export type PlanDocument = PlanEntity & Document;
+
+@Schema({
+  timestamps: true,
+  collection: 'plan',
+  toJSON: {
+    virtuals: true,
+    transform: function (doc: any, ret: any) {
+      delete ret._id_;
+      delete ret.__v;
+      return ret;
+    },
+  },
+})
+export class PlanEntity {
+  @Prop({ unique: true, type: String, default: v4 })
+  planId: string;
+  @Prop({ required: true })
+  name: string;
+  @Prop({ required: true })
+  description: string;
+  @Prop({ required: true })
+  price: number;
+  @Prop({ required: true })
+  duration: number;
+  @Prop({ required: true, type: Boolean, default: true })
+  isActive: boolean;
+  @Prop({ required: true, type: Number })
+  daysAWeek: number;
+  @Prop({
+    required: true,
+    type: ExcerciseEntity,
+    ref: 'Excercise',
+    default: [],
+  })
+  excercises: ExcerciseEntity[];
+}
+
+const PlanSchema = SchemaFactory.createForClass(PlanEntity);
+PlanSchema.virtual('id').get(function (this: PlanDocument) {
+  return this._id;
+});
+
+export { PlanSchema };
