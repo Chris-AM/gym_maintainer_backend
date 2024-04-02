046c2dec5780aa85de5c35d69890805424e622d8
Chris-AM <c.aranguizm@outlook.com>
2024-04-02 16:07:12 -0300

    :bug: solving type error

--- src/features/users/base-user/customer/domain/customer.entity.ts
+++ src/features/users/base-user/customer/domain/customer.entity.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 import { v4 } from 'uuid';
 import { BaseUserEntity } from '../../domain/base-user.entity';
 import { CoachEntity } from '../../worker-user/coach/domain/coach.entity';
@@ -23,12 +23,12 @@ export type CustomerDocument = CustomerEntity & Document;
 export class CustomerEntity extends BaseUserEntity {
   @Prop({ unique: true, type: v4 })
   customerId: string;
-  @Prop({ required: true, type: CoachEntity, ref: 'Coach' })
-  coach: CoachEntity;
+  @Prop({ required: true, type: CoachEntity, ref: 'coachUser' })
+  coach: Types.ObjectId;
   @Prop({ required: false, type: PlanEntity, ref: 'Plan', default: [] })
   plan: PlanEntity[];
   @Prop({ required: false, type: ServiceEntity, ref: 'Service', default: [] })
-  service: ServiceEntity;
+  service: Types.ObjectId;
   constructor() {
     super();
     this.type = 'customer';
