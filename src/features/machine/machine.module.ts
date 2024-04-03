8ab10cef57fdecea6ac43ce64d10d073cd54210d
Chris-AM <c.aranguizm@outlook.com>
2024-04-03 16:41:37 -0300

    :ambulance: moving from mongo to postgre

--- src/features/machine/machine.module.ts
+++ src/features/machine/machine.module.ts
@@ -1,19 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MachineService } from './use-case/machine.service';
 import { MachineController } from './use-case/machine.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { MachineEntity, MachineSchema } from './domain/machine.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MachineEntity } from './domain/machine.entity';
 
 @Module({
-  imports: [
-    MongooseModule.forFeatureAsync([
-      {
-        name: MachineEntity.name,
-        useFactory: () =>
-          MachineSchema.plugin(require('mongoose-autopopulate')),
-      },
-    ]),
-  ],
+  imports: [TypeOrmModule.forFeature([MachineEntity])],
   controllers: [MachineController],
   providers: [MachineService],
 })
