6254c0be2738d13b20d2e1627da5d6f2c139bbad
Chris-AM <c.aranguizm@outlook.com>
2024-04-04 11:23:47 -0300

    :sparkles: super user domain ready

--- src/features/users/base-user/super-user/domain/super-user.entity.ts
+++ src/features/users/base-user/super-user/domain/super-user.entity.ts
@@ -1,14 +1,17 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 import { BaseUserEntity } from '../../domain/base-user.entity';
 
 @Entity('super-user')
 export class SuperUserEntity extends BaseUserEntity {
-  
+  @PrimaryColumn('text')
   dni: string;
+  @Column('text')
   gymId: string;
+  @Column('text')
   gymName: string;
+  @Column('text')
   direction: string;
-
+  
   constructor() {
     super();
     this.type = 'super';
