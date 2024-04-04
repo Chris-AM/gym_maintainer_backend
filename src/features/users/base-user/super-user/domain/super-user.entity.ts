50088243f3f3085aafac9bfda11a32f75e04e2e3
Christian Aránguiz M <63558479+Chris-AM@users.noreply.github.com>
2024-04-04 11:28:35 -0300

    ✨ Domain layer completed (#9)
    Ready for first action

--- src/features/users/base-user/super-user/domain/super-user.entity.ts
+++ src/features/users/base-user/super-user/domain/super-user.entity.ts
@@ -1,12 +1,16 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 import { Entity } from 'typeorm';
 import { BaseUserEntity } from '../../domain/base-user.entity';
 
 @Entity('super-user')
 export class SuperUserEntity extends BaseUserEntity {
-  
+  @PrimaryColumn('text')
   dni: string;
+  @Column('text')
   gymId: string;
+  @Column('text')
   gymName: string;
+  @Column('text')
   direction: string;
 
   constructor() {
